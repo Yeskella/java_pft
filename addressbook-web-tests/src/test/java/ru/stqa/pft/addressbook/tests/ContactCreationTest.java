826b59a0175403a9b964e06b690ba6871ae56332
Yeskella <99016495+Yeskella@users.noreply.github.com>
2022-03-15 16:16:34 +0300

    1

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
@@ -2,7 +2,7 @@ package ru.stqa.pft.addressbook.tests;
 
 
 import org.testng.annotations.*;
-import ru.stqa.pft.addressbook.model.GroupDataContact;
+
 
 
 public class ContactCreationTest  extends TestBase {

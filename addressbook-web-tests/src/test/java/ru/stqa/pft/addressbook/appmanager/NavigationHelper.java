4fccd0559195c54eb0b0238f4b089440c1792a80
Yeskella <99016495+Yeskella@users.noreply.github.com>
2022-03-20 12:25:31 +0300

    Создал общий базовый класс для классов-помощников

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -3,18 +3,18 @@ package ru.stqa.pft.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class NavigationHelper {
-  public WebDriver wd;
+public class NavigationHelper extends HelperBase{
+
 
   public NavigationHelper(WebDriver wd) {
-    this.wd = wd;
+    super (wd);
   }
 
   public void gotoGroupPage() {
-    wd.findElement(By.linkText("groups")).click();
+    click(By.linkText("groups"));
   }
 
   public void logoutWeb() {
-    wd.findElement(By.linkText("Logout")).click();
+    click(By.linkText("Logout"));
   }
 }

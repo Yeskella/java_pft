c4d298b284008e44326ac552b8a8d71a1cac4e6d
Yeskella <99016495+Yeskella@users.noreply.github.com>
2022-03-20 12:08:09 +0300

    Перенес .login в SessionHelper

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,16 +1,16 @@
 package ru.stqa.pft.addressbook.appmanager;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
-  public WebDriver wd;
-  public  NavigationHelper navigationHelper;
-  public  GroupHelper groupHelper;
-  public  ContactHelper contactHelper;
+  WebDriver wd;
+  private SessionHelper sessionHelper;
+  private  NavigationHelper navigationHelper;
+  private  GroupHelper groupHelper;
+  private  ContactHelper contactHelper;
 
   public void init() {
 
@@ -20,17 +20,11 @@ public class ApplicationManager {
     groupHelper = new GroupHelper(wd);
     contactHelper = new ContactHelper(wd);
     navigationHelper = new NavigationHelper(wd);
-    login("admin", "secret");
+    sessionHelper = new SessionHelper(wd);
+    sessionHelper.login("admin", "secret");
   }
 
-  public void login(String username, String password) {
-    wd.findElement(By.name("user")).click();
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys(username);
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys(password);
-    wd.findElement(By.xpath("//input[@value='Login']")).click();
-  }
+
 
   public void stop() {
     wd.quit();
